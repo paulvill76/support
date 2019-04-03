9fe0c13ec5b9c0c266af305bc58466585dd3aa32
Elliott Panipinto <elliott.panipinto@jumpcloud.com>
2019-04-03 10:28:04 -0600

    add invoke-git

--- PowerShell/Deploy/Functions/Invoke-GitCommit.ps1
+++ PowerShell/Deploy/Functions/Invoke-GitCommit.ps1
@@ -3,23 +3,20 @@ Function Invoke-GitCommit
     param (
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$BranchName
     )
+    # Install BuildHelpers module and import commands used
+    If (!(Get-Module -Name:('BuildHelpers') -ListAvailable)) { Install-Module -Name:('BuildHelpers') -Force }
+    If (Get-Module -Name:('BuildHelpers') -ListAvailable) { Import-Module -Name:('BuildHelpers') -Function:('Invoke-Git') }
+    # Logging
     $MyName = $MyInvocation.MyCommand.Name
     $ParentScriptName = (Get-PSCallStack).Where( {$_.Command -notin ($MyName, $MyName.Replace('.ps1', ''))}).Command -join ','
     $CommitMessage = 'Push to ' + $BranchName + '; Called by:' + $ParentScriptName + ';'
     $UserEmail = 'AzurePipelines@FakeEmail.com'
     $UserName = 'AzurePipelines'
-    Write-Host ('[status]Git version')
-    git --version
-    Write-Host ('[status]Set git user email to: ' + $UserEmail)
-    git config user.email $UserEmail
-    Write-Host ('[status]Set git user name to: ' + $UserName)
-    git config user.name $UserName
-    Write-Host ('[status]Check git status')
-    git status
-    Write-Host ('[status]Git add all uncommitted changes')
-    git add -A
-    Write-Host ('[status]Git commit all changes with message: ' + $CommitMessage)
-    git commit -a -m $CommitMessage
-    Write-Host ('[status]Git push to: ' + $BranchName)
-    git push --force origin HEAD:refs/heads/$BranchName
+    Invoke-Git -Arguments:('--version')
+    Invoke-Git -Arguments:('config user.email ' + $UserEmail)
+    Invoke-Git -Arguments:('config user.name ' + $UserName)
+    Invoke-Git -Arguments:('status')
+    Invoke-Git -Arguments:('add -A')
+    Invoke-Git -Arguments:('commit -a -m ' + $CommitMessage)
+    Invoke-Git -Arguments:('push --force origin HEAD:refs/heads/' + $BranchName)
 }
--- PowerShell/Deploy/Functions/Invoke-GitMerge.ps1
+++ PowerShell/Deploy/Functions/Invoke-GitMerge.ps1
@@ -4,8 +4,12 @@ Function Invoke-GitMerge
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 0)][ValidateNotNullOrEmpty()][string]$SourceBranch,
         [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true, Position = 1)][ValidateNotNullOrEmpty()][string]$TargetBranch
     )
+    # Install BuildHelpers module and import commands used
+    If (!(Get-Module -Name:('BuildHelpers') -ListAvailable)) { Install-Module -Name:('BuildHelpers') -Force }
+    If (Get-Module -Name:('BuildHelpers') -ListAvailable) { Import-Module -Name:('BuildHelpers') -Function:('Invoke-Git') }
+    # Logging
     $MyName = $MyInvocation.MyCommand.Name
-    $ParentScriptName = (Get-PSCallStack).Where( {$_.Command -notin ($MyName, $MyName.Replace('.ps1', ''))}).Command -join ','
+    $ParentScriptName = (Get-PSCallStack).Where( { $_.Command -notin ($MyName, $MyName.Replace('.ps1', '')) }).Command -join ','
     $CommitMessage = 'Merge ' + $SourceBranch + ' into ' + $TargetBranch + '; Called by:' + $ParentScriptName + ';'
     Write-Host ('[status]SOURCE BRANCH:' + $SourceBranch + ';TARGET BRANCH:' + $TargetBranch + ';')
     If ($SourceBranch -eq 'refs/heads/' + $TargetBranch )
@@ -14,21 +18,13 @@ Function Invoke-GitMerge
     }
     Else
     {
-        Write-Host ('[status]GIT FETCH ORIGIN')
-        git fetch origin
-        Write-Host ('[status]GIT CHECKOUT ' + $SourceBranch)
-        git checkout $SourceBranch
-        Write-Host ('[status]GIT CHECKOUT ' + $TargetBranch)
-        git checkout $TargetBranch
-        Write-Host ('[status]GIT STATUS')
-        git status
-        Write-Host ('[status]GIT MERGE ' + $SourceBranch + ' -m ' + $CommitMessage)
-        git merge $SourceBranch -m $CommitMessage
-        Write-Host ('[status]GIT STATUS')
-        git status
-        Write-Host ('[status]GIT PUSH ORIGIN')
-        git push origin
-        Write-Host ('[status]GIT STATUS')
-        git status
+        Invoke-Git -Arguments:('fetch origin')
+        Invoke-Git -Arguments:('checkout ' + $SourceBranch)
+        Invoke-Git -Arguments:('checkout ' + $TargetBranch)
+        Invoke-Git -Arguments:('status')
+        Invoke-Git -Arguments:('merge ' + $SourceBranch + ' -m ' + $CommitMessage)
+        Invoke-Git -Arguments:('status')
+        Invoke-Git -Arguments:('push origin')
+        Invoke-Git -Arguments:('status')
     }
 }
